8234b004deb86648b5b86ded1c87ec4612ac69c2
Kitsune Nishizono <rrangel@hub-camp.mx>
2018-02-10 20:12:43 -0600

    Se cambia leyenda en casos nulos para hacerla más entendible

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 var edad = parseInt(prompt("¿Qué edad tienes?"));
 
 function edad_cliente(edad){
-	if(!edad) return "No es una edad válida";
+	if(!edad) return "Sólo se admiten números.";
 	if (edad >= 18) return "Puedes entrar a la página";
 	else return "Eres muy joven para entrar a la página";
 }
